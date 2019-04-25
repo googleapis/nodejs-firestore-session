83b66785349916c95a0a1094db2897807ca05c6d
F. Hinkelmann <franzih@chromium.org>
2019-04-25 13:30:53 -0400

    test: Add unit tests (#9)
    Fixes https://github.com/googleapis/nodejs-firestore-session/issues/3

--- test/test.js
+++ test/test.js
@@ -15,7 +15,107 @@
 
 'use strict';
 
-it('should work', done => {
-  console.warn('no unit tests available 👻');
+const assert = require('assert');
+const session = require('express-session');
+
+const FirestoreStore = require('../src/index')(session);
+
+it('should throw without dataset', done => {
+  assert.throws(() => new FirestoreStore());
   done();
+});
+
+it('should use the default kind', done => {
+  const fakeDataset = {};
+  fakeDataset.collection = kind => {
+    assert.strictEqual(kind, 'Session');
+    done();
+  };
+
+  const store = new FirestoreStore({dataset: fakeDataset});
+  store.get();
+});
+
+it('should use the provided kind', done => {
+  const expectedkind = 'providedKind';
+  const fakeDataset = {};
+  fakeDataset.collection = kind => {
+    assert.strictEqual(kind, expectedkind);
+    done();
+  };
+
+  const store = new FirestoreStore({
+    dataset: fakeDataset,
+    kind: expectedkind,
+  });
+  store.get();
+});
+
+it('should get a document', done => {
+  const fakeDataset = {
+    collection: kind => {
+      assert.strictEqual(kind, 'Session');
+      return fakeDataset;
+    },
+    doc: sid => {
+      assert.strictEqual(sid, expectedSid);
+      return fakeDataset;
+    },
+    get: () => {
+      done();
+    },
+  };
+
+  const store = new FirestoreStore({
+    dataset: fakeDataset,
+  });
+
+  const expectedSid = 'sid';
+  store.get(expectedSid);
+});
+
+it('should set a document', done => {
+  const fakeDataset = {
+    collection: kind => {
+      assert.strictEqual(kind, 'Session');
+      return fakeDataset;
+    },
+    doc: sid => {
+      assert.strictEqual(sid, expectedSid);
+      return fakeDataset;
+    },
+    set: () => {
+      done();
+    },
+  };
+
+  const store = new FirestoreStore({
+    dataset: fakeDataset,
+  });
+
+  const expectedSid = 'sid';
+  store.set(expectedSid);
+});
+
+it('should destroy a document', done => {
+  const fakeDataset = {
+    collection: kind => {
+      assert.strictEqual(kind, 'Session');
+      return fakeDataset;
+    },
+    doc: sid => {
+      assert.strictEqual(sid, expectedSid);
+      return fakeDataset;
+    },
+    delete: () => {
+      done();
+    },
+  };
+
+  const store = new FirestoreStore({
+    dataset: fakeDataset,
+  });
+
+  const expectedSid = 'sid';
+  store.destroy(expectedSid);
 });
